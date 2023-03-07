78c7e0abab066061ebe60f4affebff87d32f6a46
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-03-07 14:12:55 +0100

    added the auth page

---
+++ pages/auth.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import React from "react";
+
+interface Props {}
+
+function Auth(props: Props) {
+  const {} = props;
+
+  return (
+    <div className="">
+ <header>
+        <nav className="flex justify-between gap-2 items-center p-4">
+
+        <Link href="/" className="text-blue-400">Home</Link>
+        <Link href="/auth" className="text-blue-400">Login</Link>
+        </nav>
+
+      </header>
+      <main className="flex flex-col min-h-screen bg-gray-800">
+      <div className="">
+        <div className="flex  self-center my-20 justify-center">
+          <form className="flex flex-col gap-2 p-10 bg-white shadow-md">
+            <h1 className="text-2xl font-bold">Login</h1>
+            <InputComponent type="email" placeholder="Email" />
+            <InputComponent type="password" placeholder="Password" />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+
+    </div>
+    
+  );
+}
+
+function InputComponent(props: any) {
+  const { type, placeholder, value, onChange } = props;
+  return (
+    <div className="flex flex-col gap-1 text-left">
+      <label htmlFor={placeholder}>{placeholder}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="border-2 border-gray-100 outline-none rounded-md w-fit p-2"
+      />
+    </div>
+  );
+}
+
+export default Auth;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import React from "react";
+import JSONPretty from "react-json-pretty";
+import Link from "next/link";
 
 export default function Home() {
+  const [data, setData] = React.useState("");
+
+  const generateData = () => {
+    fetch("https://fakerapi.it/api/v1/persons?_quantity=1")
+      .then((res) => res.json())
+      .then((data) => {
+        const finalData = data.data[0];
+        console.log(finalData);
+        setData(finalData);
+      });
+  };
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(JSON.stringify(data));
+  };
   return (
-    <div className="bg-gray-900 text-white w-full h-screen flex flex-col items-center">
+    <div className="bg-gray-900 text-white w-full h-screen items-center">
       <Head>
         <title>Fictional Data generator</title>
         <meta
@@ -13,7 +30,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="font-sans text-center py-6 ">
+      <header>
+        <nav className="flex justify-between gap-2 items-center p-4">
+
+        <Link href="/" className="text-blue-400">Home</Link>
+        <Link href="/auth" className="text-blue-400">Login</Link>
+        </nav>
+
+      </header>
+      <main className="flex flex-col font-sans text-center py-6   ">
         <h1 className="text-3xl font-bold"> Data Generator</h1>
         <p className="text-gray-400">
           The revolutionary software used for generating high-quality data for
@@ -26,18 +51,28 @@ export default function Home() {
             className="p-2 outline-none focus:text-black"
             placeholder="Type a prompt e.g generate a full user details for signup"
           />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            onClick={generateData}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
             Generate
           </button>
         </div>
         <p className="text-gray-400 font-bold">Generated Data</p>
 
-        <div className="relative m-4">
-          <div className="flex flex-col text-left bg-gray-800 p-4 rounded-lg">
-            <p className="text-gray-200">
-              {"{name: 'John Doe', email: 'joe@gmail.com '}"}
-            </p>
-            <button className="absolute top-2 right-2 bg-blue-400 outline-none hover:bg-blue-600 p-2 text-xs rounded-md">
+        <div className="flex flex-col relative m-4 w-2/3 justify-center  self-center">
+          <div className="flex flex-col  text-left bg-gray-800 p-4 h-40 w-full overflow-auto rounded-lg scrollbar">
+            <div className="text-gray-300">
+              {data ? (
+                <JSONPretty id="json-pretty" data={data}></JSONPretty>
+              ) : (
+                "No data generated yet"
+              )}
+            </div>
+            <button
+              onClick={copyToClipboard}
+              className="absolute top-2 right-2 bg-blue-400 outline-none hover:bg-blue-600 p-2 text-xs rounded-md"
+            >
               Copy
             </button>
           </div>
