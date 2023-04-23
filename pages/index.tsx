3f23ad94762c0f73d230ec0c08ef3a514d29e375
Lukmon Abdulsalam <40471908+litmus-zhang@users.noreply.github.com>
2023-04-23 07:57:58 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,6 +70,7 @@ export default function Home() {
           />
           <button
             onClick={generateData}
+            disabled={loading}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             {loading ? "Generating..." : "Generate"}
